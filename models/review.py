8edecfe87630979291475da8cd11f98ff510e9ee
maqamylee0 <maqamylee0@gmail.com>
2024-01-16 15:43:17 +0300

    fix review foreignkeys

--- models/review.py
+++ models/review.py
@@ -9,8 +9,8 @@ from sqlalchemy import Column, String, ForeignKey
 class Review(BaseModel, Base):
     """ Review classto store review information """
 
-    __tablename__ = 'review'
+    __tablename__ = 'reviews'
 
-    place_id = Column(String(1024), nullable=False)
-    user_id = Column(String(60), ForeignKey("places.id"), nullable=False)
-    text = Column(String(60), ForeignKey("users.id"), nullable=False)
+    text = Column(String(1024), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+    place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
