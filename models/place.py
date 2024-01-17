f9c39247797f4b870c521fcc0a31413d0a234e72
maqamylee0 <maqamylee0@gmail.com>
2024-01-17 13:12:27 +0300

    implement place amenities table

--- models/place.py
+++ models/place.py
@@ -3,10 +3,29 @@
 
 
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Integer, Float, ForeignKey
+from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
 from sqlalchemy.orm import relationship
 from os import getenv
 import os
+from models.amenity import Amenity
+
+
+class PlaceAmenity(BaseModel, Base):
+    """PlaceAmenity class"""
+    __tablename__ = 'place_amenity'
+
+    place_id = Column(String(60),
+                      ForeignKey('places.id'),
+                      primary_key=True,
+                      nullable=False)
+    amenity_id = Column(String(60),
+                        ForeignKey('amenities.id'),
+                        primary_key=True,
+                        nullable=False)
+# place_amenity = Table('place_amenity', Base.metadata,
+#     Column('place_id', Integer, ForeignKey('places.id')),
+#     Column('amenity_id', Integer, ForeignKey('amenities.id'))
+# )
 
 
 class Place(BaseModel, Base):
@@ -27,7 +46,9 @@ class Place(BaseModel, Base):
         latitude = Column(Float(), nullable=False)
         longitude = Column(Float(), nullable=False)
         reviews = relationship("Review", backref="place", cascade="delete")
-
+        amenities = relationship("Amenity",
+                                 secondary="place_amenity",
+                                 viewonly=False)
         if getenv('HBNB_TYPE_STORAGE') != 'db':
 
             @property
@@ -54,3 +75,23 @@ class Place(BaseModel, Base):
         longitude = 0.0
         reviews = []
         amenity_ids = []
+
+        @property
+        def amenities(self):
+            """Get amenities if storage if filestorage"""
+            from models import storage
+            from models.amenity import Amenity
+            all_amenities = storage.all(Amenity)
+            plc_amenities = []
+            for a in all_amenities.values():
+                if a.id in self.amenity_ids:
+                    plc_amenities.append(a)
+            return plc_amenities
+
+        @amenities.setter
+        def amenities(self, obj):
+            """Set amenities if storage if filestorage"""
+            from models import storage
+            from models.amenity import Amenity
+            if type(obj) == Amenity:
+                self.amenity_ids.append(obj.id)
