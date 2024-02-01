e5757f3bbf7252df3f59206680a0647532314a10
maqamylee0 <maqamylee0@gmail.com>
2024-02-01 11:52:43 -0800

    fix styling

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
-#script to set up servers for web static deployment
+# script to set up servers for web static deployment
 
 sudo apt-get update
 sudo apt-get -y install nginx
-sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-echo "<html><head></head><body>Hello World!</body></html>" | sudo tee /data/web_static/releases/test/index.htmlsudo ln -sf /data/web_static/releases/test /data/web_static/current
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared3
+echo "<html><head></head><body>Hello World!</body></html>" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data
 nginx_config="/etc/nginx/sites-available/default"
 sudo sed -i "/server_name _;/a \\\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}" $nginx_config
--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -6,7 +6,6 @@ from datetime import datetime
 def do_pack():
     """function to archive contents of web static"""
     try:
-        
         local('mkdir -p versions')
         timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
         archive_name = 'web_static_{}.tgz'.format(timestamp)
