8cdd4fe60e80817f493224256c4334d7ac82252c
zagaouch <i.zagaouch@gmail.com>
2024-02-05 02:06:47 +0100

    auto commit

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -8,24 +8,22 @@ env.hosts = ['54.236.12.67', '54.90.47.94']
 env.user = 'ubuntu'
 env.key = '~/.ssh/id_rsa'
 
-
 def do_deploy(archive_path):
-    """deploys archived files to server"""
-    if not exists(archive_path):
+    """distributes an archive to the web servers"""
+    if exists(archive_path) is False:
         return False
     try:
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
+        path = "/data/web_static/releases/"
         put(archive_path, '/tmp/')
-        archive_filename = archive_path.split('/')[-1]
-        webstatic = '/data/web_static/releases/{}'
-        release_folder = webstatic.format(archive_filename.split('.')[0])
-        run('sudo mkdir -p {}'.format(release_folder))
-        run('sudo tar -xzf /tmp/{} -C {}'.format(archive_filename,
-                                                 release_folder))
-        run('sudo rm /tmp/{}'.format(archive_filename))
-        run('sudo mv {}/web_static/* {}'.format(release_folder,
-                                                release_folder))
-        run('sudo rm -rf /data/web_static/current')
-        run('sudo ln -s {} /data/web_static/current'.format(release_folder))
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
         return True
-    except Exception as e:
+    except:
         return False
