f719a69d6cefb443820d33fb3a77c5a87900e604
zagaouch <i.zagaouch@gmail.com>
2024-02-16 21:59:03 +0100

    auto commit

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -31,11 +31,11 @@ def pythoniscool(text='is cool'):
     """display “Python ”, followed by the value of the text variable"""
     return 'Python ' + text.replace('_', ' ')
 
+
 @app.route('/number/<int:n>', strict_slashes=False)
 def imanumber(n):
     """display “n is a number” only if n is an integer"""
     return "{:d} is a number".format(n)
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
