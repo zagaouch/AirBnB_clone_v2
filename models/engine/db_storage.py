250e73dbb42a93357505f10bd130bb28191d3fa7
zagaouch <i.zagaouch@gmail.com>
2024-02-18 20:55:30 +0100

    auto commit

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -86,3 +86,7 @@ class DBStorage:
         Base.metadata.create_all(self.__engine)
         Session = sessionmaker(bind=self.__engine, expire_on_commit=False)
         self.__session = scoped_session(Session)
+
+    def close(self):
+        """call remove() method on the private session attribute"""
+        self.__session.remove()
